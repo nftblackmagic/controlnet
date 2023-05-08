8b0c4e8994fc13041e192a5342e5f3aa12abd6d1
An <anzhanghust@gmail.com>
2023-05-07 17:01:19 -0700

    1.1 upgrade

--- utils.py
+++ utils.py
@@ -2,14 +2,14 @@ import os
 from subprocess import call
 
 model_dl_urls = {
-    "canny": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_canny.pth",
-    "depth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_depth.pth",
-    "hed": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_hed.pth",
-    "normal": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_normal.pth",
-    "mlsd": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_mlsd.pth",
-    "openpose": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_openpose.pth",
-    "scribble": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_scribble.pth",
-    "seg": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_seg.pth",
+    "canny": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_canny.pth",
+    "depth": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11f1p_sd15_depth.pth",
+    "softedge": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_softedge.pth",
+    "normal": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_normalbae.pth",
+    "mlsd": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_mlsd.pth",
+    "openpose": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_openpose.pth",
+    "scribble": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_scribble.pth",
+    "seg": "https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_seg.pth",
 }
 
 annotator_dl_urls = {
@@ -27,7 +27,7 @@ def download_model(model_name, urls_map):
     Download model from huggingface with wget and save to models directory
     """
     model_url = urls_map[model_name]
-    relative_path_to_model = model_url.replace("https://huggingface.co/lllyasviel/ControlNet/resolve/main/", "")
+    relative_path_to_model = model_url.replace("https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/", "")
     if not os.path.exists(relative_path_to_model):
         print(f"Downloading {model_name}...")
         call(["wget", "-O", relative_path_to_model, model_url])
@@ -36,4 +36,4 @@ def get_state_dict_path(model_name):
     """
     Get path to model state dict
     """
-    return f"./models/control_sd15_{model_name}.pth"
+    return f"./models/control_v11p_sd15_{model_name}.pth"
